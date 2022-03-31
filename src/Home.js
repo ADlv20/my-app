bcd5a850d3d037fba42bb4fbb439f82806e23704
ADlv19 <adwaitg02@gmail.com>
2022-03-31 19:48:33 +0530

    NM

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
+    
+    const [name , setName ] = useState("Adwait");
+
+    const handleClick = () => {
+        console.log(name);
+        setName('Gawade');
+    }
+
     return ( 
         <div className="home">
-            HomePage
+            <h1>Homepage</h1>
+            <p> { name }</p>
+            <button onClick={handleClick}>Click</button>
         </div>
      );
 }
