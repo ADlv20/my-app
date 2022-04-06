80e20f293bd7617ebc3c53c181c89f05cee57972
ADlv19 <adwaitg02@gmail.com>
2022-04-06 22:34:06 +0530

    nm

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ function App() {
        <Navbar />
        <div className='content'>
           <Home />
-       </div>
+      </div>
+      <iframe src="https://drive.google.com/file/d/1GkBsVNDn2kjWT-1XxznIBFaFtLeO3tPE/preview" width="640" height="480" allow="autoplay"></iframe>
+      
     </div>
 
   );
